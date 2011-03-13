5f9ff6937e271acead5ff0a8bb97e760494a4aa6
Kiril Vladimiroff <v.kiril@gmail.com>
2011-03-13 03:23:07 +0200

    Comments added

--- vouchers_generator.py
+++ vouchers_generator.py
@@ -1,9 +1,14 @@
 from random import choice
 
-existed = ['BA1954CE', 'CACA43BB', 'AE4C56CD']
+existed = ['BA1954CE', 'CACA43BB', 'AE4C56CD'] #sample codes
 pattern = '1234567890ABCDEF'
 
 def generate(count = 1, length = 8):
+    '''
+    Little function used for generating voucher codes for the PyFMI course.
+    count argument represents how many codes should be generated and
+    length stands for the code's length.
+    '''
     generated = []
     fails = 0
     for code in range(count):
