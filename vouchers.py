c336a542d1fba21045898029c222f2ad154b05dc
Dimitar Dimitrov <me@ddimitrov.name>
2014-10-08 16:54:41 +0300

    Use utf-8 to fix encoding errors

--- vouchers.py
+++ vouchers.py
@@ -23,7 +23,7 @@ def generate(count=1, existed=[], length=8, pattern='0123456789ABCDEF'):
         generated += generate(fails)
     return generated
 
-def to_html(codes=[], days_valid=15, output='index.html'):
+def to_html(codes=[], days_valid=15, output='index.html', encoding='utf-8'):
     '''
     Convert generated vouchers in a html page, so it could be printed out
     `codes[]` takes the generated codes
@@ -32,12 +32,12 @@ def to_html(codes=[], days_valid=15, output='index.html'):
     '''
     html_code = ''
     valid_until = (date.today() + timedelta(days = days_valid)).strftime('%d.%m.%Y')
-    with open('html/header.html', 'r') as header:
+    with open('html/header.html', 'r', encoding = encoding) as header:
         html_code += header.read()
     for code in codes:
         html_code += '<li><h1 class="code">{}</h1></li>'.format(str(code))
-    with open('html/footer.html', 'r') as footer:
+    with open('html/footer.html', 'r', encoding = encoding) as footer:
         html_code += footer.read()
-    with open('html/index.html', 'w') as file:
+    with open('html/index.html', 'w', encoding = encoding) as file:
         file.write(html_code)
 
